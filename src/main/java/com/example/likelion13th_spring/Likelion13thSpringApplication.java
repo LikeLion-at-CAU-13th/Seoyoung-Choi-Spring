079ef931454e1058bd9eaf8bc8669f8d3f1db3c7
dallaechoi <choi032799@gmail.com>
2025-07-21 00:14:56 +0900

    week 16 upload

--- src/main/java/com/example/likelion13th_spring/Likelion13thSpringApplication.java
+++ src/main/java/com/example/likelion13th_spring/Likelion13thSpringApplication.java
@@ -2,6 +2,9 @@ package com.example.likelion13th_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Likelion13thSpringApplication {
